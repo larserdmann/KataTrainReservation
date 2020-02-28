9f525ac32c23d3afeced5704ab3ac903f6d74a85
Nick <nick.mueller@uni-greifswald.de>
2020-02-28 13:14:24 +0100

    * Adds wiremock to mock rest server * Begins implementation TicketOffice.makeReservation

--- Java/src/main/java/TicketOffice.java
+++ Java/src/main/java/TicketOffice.java
@@ -1,8 +1,31 @@
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.util.List;
+
 public class TicketOffice {
-    
+
+    private String baseUrl;
+
+    public TicketOffice(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
     public Reservation makeReservation(ReservationRequest request) {
-		//TODO: implement this code!
-		return null;
+
+        // Call booking_reference api here
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(UriBuilder.fromUri(baseUrl).path("booking_reference"));
+
+        Response response = target.request().get();
+
+        String bookingId = response.readEntity(String.class);
+        List<Seat> seats = null;
+
+
+        return new Reservation(request.trainId, seats, bookingId);
     }
 
 }
