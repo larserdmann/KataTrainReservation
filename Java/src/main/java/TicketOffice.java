6ca0735ef6bba034ddf9b864cc88cc441d8413e4
Nick <nick.mueller@uni-greifswald.de>
2020-03-06 12:47:04 +0100

    integrates train-data-server

--- Java/src/main/java/TicketOffice.java
+++ Java/src/main/java/TicketOffice.java
@@ -1,31 +1,51 @@
+import train.TrainData;
+import train.TrainDataParser;
+
+import javax.json.JsonObject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import java.util.List;
+import java.util.Set;
 
 public class TicketOffice {
 
-    private String baseUrl;
+    private String bookingRefBaseUrl;
+    private String trainDataBaseUrl;
+    private TrainDataParser trainDataParser;
 
-    public TicketOffice(String baseUrl) {
-        this.baseUrl = baseUrl;
+    public TicketOffice(String baseUrl, String trainDataBaseUrl, TrainDataParser trainDataParser) {
+        this.bookingRefBaseUrl = baseUrl;
+        this.trainDataBaseUrl = trainDataBaseUrl;
+        this.trainDataParser = trainDataParser;
     }
 
     public Reservation makeReservation(ReservationRequest request) {
 
-        // Call booking_reference api here
+        String bookingId = createBookingId();
+
         Client client = ClientBuilder.newClient();
-        WebTarget target = client.target(UriBuilder.fromUri(baseUrl).path("booking_reference"));
+        WebTarget target = client.target(UriBuilder.fromUri(trainDataBaseUrl).path("data_for_train").path(request.trainId));
+
+        Response response = target.request().get();
+        JsonObject json = response.readEntity(JsonObject.class);
+
+        TrainData trainData = trainDataParser.parse(json);
+
+        List<Seat> seats = null;
+
+        return new Reservation(request.trainId, seats, bookingId);
+    }
+
+    private String createBookingId() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(UriBuilder.fromUri(bookingRefBaseUrl).path("booking_reference"));
 
         Response response = target.request().get();
 
-        String bookingId = response.readEntity(String.class);
-        List<Seat> seats = null;
-
-
-        return new Reservation(request.trainId, seats, bookingId);
+        return response.readEntity(String.class);
     }
 
 }
--- Java/src/test/java/TicketOfficeTest.java
+++ Java/src/test/java/TicketOfficeTest.java
@@ -4,30 +4,58 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import train.TrainData;
+import train.TrainDataParser;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static com.github.jenspiegsa.wiremockextension.ManagedWireMockServer.with;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 @DisplayName("WireMockExtension usage")
 @ExtendWith(WireMockExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class TicketOfficeTest {
 
     @Managed
-    WireMockServer server = with(wireMockConfig().dynamicPort());
-    private String baseUrl;
+    WireMockServer bookingRefServer = with(wireMockConfig().dynamicPort());
+    @Managed
+    WireMockServer trainDataServer = with(wireMockConfig().dynamicPort());
+    @Mock
+    TrainDataParser trainDataParser;
+
+    private String bookingRefBaseUrl;
+    private String trainDataBaseUrl;
 
     @Test
     public void reserveSeats() {
 
         String expectedBookingId = "75bcd15";
-        givenThat(get("/booking_reference").willReturn(ok(expectedBookingId)));
+        String json = fromFile("express_2000_response.json");
 
-        baseUrl = server.baseUrl();
+        bookingRefServer.givenThat(get("/booking_reference").willReturn(ok(expectedBookingId)));
+        trainDataServer.givenThat(get("/data_for_train/express_2000").willReturn(okJson(json)));
 
-        ReservationRequest request = new ReservationRequest("",123);
-        TicketOffice ticketOffice = new TicketOffice(baseUrl);
+        given(trainDataParser.parse(any())).willReturn(new TrainData());
+
+        bookingRefBaseUrl = bookingRefServer.baseUrl();
+        trainDataBaseUrl = trainDataServer.baseUrl();
+
+        ReservationRequest request = new ReservationRequest("express_2000", 123);
+        TicketOffice ticketOffice = new TicketOffice(bookingRefBaseUrl, trainDataBaseUrl, trainDataParser);
 
 
         Reservation reservation = ticketOffice.makeReservation(request);
@@ -35,5 +63,17 @@ public class TicketOfficeTest {
 
         then(reservation).isNotNull();
         then(reservation.bookingId).isEqualTo(expectedBookingId);
+    }
+
+
+    String fromFile(final String filename) {
+        try {
+            final URI uri = getClass().getResource(filename).toURI();
+            return new String(Files.readAllBytes(Paths.get(uri)), StandardCharsets.UTF_8);
+        } catch (final URISyntaxException e) {
+            throw new RuntimeException(e);
+        } catch (final IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 }
