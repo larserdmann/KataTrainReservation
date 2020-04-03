8f12d4264b3c7cdb48385ca972ee7e4b23a053a4
larserdmann <lars.erdmann@mail.de>
2020-04-03 14:30:30 +0200

    first rule of train reservation

--- Java/src/main/java/train/ReservationService.java
+++ Java/src/main/java/train/ReservationService.java
@@ -1,11 +1,19 @@
 package train;
 
+import static java.util.Collections.emptyList;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReservationService {
+
     // result in list of free seats
     public List<Seat> tryToReserve(final ReservationRequest request, final TrainData trainData, String bookingReference) {
+
+        if (trainData == null || isMaximumTrainThresholdExceeded(request, trainData)) {
+            return emptyList();
+        }
+
         List<Seat> seatsToReserve = new ArrayList<>();
 
         for (int i = 0; i < request.seatCount; i++) {
@@ -20,6 +28,13 @@ public class ReservationService {
         }
 
         return seatsToReserve;
+    }
+
+    private boolean isMaximumTrainThresholdExceeded(final ReservationRequest request, final TrainData trainData) {
+        final int resultingSeatCount = request.seatCount + trainData.getReservedSeatCount();
+        final double seventyPercentSeatCount = 0.7 * trainData.getTotalSeatCount();
+
+        return resultingSeatCount > seventyPercentSeatCount;
     }
 
     // reservation is done in ticket office
